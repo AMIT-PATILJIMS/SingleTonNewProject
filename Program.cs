e74f806607cfa54ad658fbf5afbcce8a324e093d
AMIT-PATILJIMS <ap1079997@gmail.com>
2024-01-03 19:13:25 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace SingleTonNewProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SingletonDemo fromEmplyee = SingletonDemo.GetInsurance;
+
+            fromEmplyee.PrintDetails("From Employee");
+
+            SingletonDemo fromStudent = SingletonDemo.GetInsurance;
+
+            fromStudent.PrintDetails("From student");
+        }
+    }
+}
---
+++ SingletonDemo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleTonNewProject
+{
+    internal sealed class SingletonDemo
+    {
+        private static int counter = 0;
+
+        private static SingletonDemo instance = null;
+        public static SingletonDemo GetInsurance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new SingletonDemo();
+                }
+                return instance;
+            }
+        }
+
+        private SingletonDemo()
+        {
+            counter++;
+            Console.WriteLine("counter value is " + counter.ToString());
+        }
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
